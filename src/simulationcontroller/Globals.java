62c4c3f776742c80d94eb7ed48be883eb06cc94d
Ahmed Alnahdi <111814199+AhmedAlnahdi@users.noreply.github.com>
2026-01-07 14:09:55 +0300

    Update Globals.java

--- src/simulationcontroller/Globals.java
+++ src/simulationcontroller/Globals.java
@@ -7,12 +7,9 @@ import java.util.List;
 
 /*
     Global scheduling metrics shared across the system.
-
-    According to the Project Proposal, the scheduler requires:
-      - SR:  Sum of remaining burst times of all processes in the ready queue.
-
+    - SR:  Sum of remaining burst times of all processes in the ready queue.
     AR (Average Remaining time) was originally meant to be stored globally,  
-    but we redesigned it to be computed locally inside DRoundRobinScheduler.
+    but redesigned it to be computed locally inside DRoundRobinScheduler.
     So it prevents accidental global updates and keeps AR consistent with the 
     currently selected process and ready queue snapshot.
 */
